344755882d1fdd3e692f0d00b8ff1378b9922a2a
joao <joaoeifler2004@gmail.com>
2022-05-25 13:51:46 -0400

    Finished round function

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function computerPlay() {
-    let plays = ["Rock", "Paper", "Scissors"];
+    let plays = ["rock", "paper", "scissors"];
     return plays[Math.floor(Math.random() * plays.length)];
 }
 
+function round() {
+    let playerSelection = "";
+    while (playerSelection == "") {
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+    }
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay();
+    
+    playerPoints = 0;
+    computerPoints = 0;
+    
+    if (playerSelection === computerSelection) return "tie";
+    if (playerSelection == "rock" && computerSelection == "scissors") return "player";
+    if (playerSelection == "rock" && computerSelection == "paper") return "computer";
+    if (playerSelection == "paper" && computerSelection == "rock") return "player";
+    if (playerSelection == "paper" && computerSelection == "scissors") return "computer";
+    if (playerSelection == "scissors" && computerSelection == "paper") return "player";
+    if (playerSelection == "scissors" && computerSelection == "rock") return "computer"; 
+}
